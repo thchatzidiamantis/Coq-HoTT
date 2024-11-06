c9745a6800c09c8e89bc0b3f6e5b5db732e89d7f
thchatzidiamantis <tchtc@proton.me>
2024-11-06 13:32:54 -0500

    Uniform continuity proof completed

--- theories/BarInduction.v
+++ theories/BarInduction.v
@@ -2,6 +2,10 @@ Require Import Basics Types.
 Require Import Truncations.Core.
 Require Import Spaces.Nat.Core.
 Require Import Sequences.
+Require Import Basics Types.
+Require Import Truncations.Core.
+Require Import Spaces.Nat.Core.
+Require Import Sequences.
 
 Open Scope nat_scope.
 Open Scope type_scope.
@@ -34,50 +38,87 @@ Proof.
   contradiction (not_lt_zero_r i).
 Defined.
 
+Definition seq_res_eq_iff {A : Type} {n : nat} {u v : nat -> A}
+  : (restrict u n == restrict v n) <-> (forall (m : nat), m < n -> u m = v m)
+  := (fun h m hm => h (m; hm), fun h '(m; hm) => h m hm).
+
+Definition seq_agree_res {A : Type} {n : nat} {u v : nat -> A} 
+  (h : restrict u n == restrict v n)
+  : u =[n] v.
+Proof.
+  induction n in u, v, h |-*.
+  - exact tt.
+  - constructor.
+    + rapply (h (0 ; _)).
+      exact _.
+    + apply IHn.
+      intros (k , hk).
+      exact (h (k.+1 ; lt_succ hk)).
+Defined.
+
+(** This could be rephrased and put into the sequences file (i.e. u =[n] v -> forall m < n, u m = v m). *)
+Definition res_seq_agree {A : Type} {n : nat} {u v : nat -> A} 
+  (h : u =[n] v)
+  : restrict u n == restrict v n.
+Proof.
+  intros (m , hm).
+  induction n in u, v, m, h, hm |-*.
+  - contradiction (not_lt_zero_r _ hm).
+  - induction m in u, v, h, hm |-*.
+    + exact (fst h).
+    + rapply (IHn (tail u) (tail v) (snd h)).
+Defined.
+
+Definition res_seq_agree' `{Funext} {A : Type} {n : nat} {u v : nat -> A} 
+  (h : u =[n] v)
+  : restrict u n = restrict v n
+  := path_forall _ _ (res_seq_agree h).
+
+Definition seq_agree_iff_res {A : Type} {n : nat} {u v : nat -> A}
+  : (u =[n] v) <-> restrict u n == restrict v n
+  := (fun h => res_seq_agree h, fun h => seq_agree_res h).  
+
 (** We are not using this definition yet, as it clashes with the implicit {n : nat} in our predicate.  *)
 Definition is_bar {A : Type} (B : forall {n : nat} (s : Seq A n), Type)
   := forall (s : nat -> A), {n : nat & B (restrict s n)}.
 
 Definition decidable_bar_induction (A : Type) :=
   forall (B : forall {n : nat} (s : Seq A n), Type)
-(* jdc: isprop would be better.  "pred" is often used for predecessor. *)
-  (ispred : forall {n : nat} (s : Seq A n), IsHProp (B s))
+  (isprop : forall {n : nat} (s : Seq A n), IsHProp (B s))
   (dec : forall {n : nat} (s : Seq A n), Decidable (B s))
   (ind : forall (n : nat) (s : Seq A n), (forall (a : A), B (append s a)) -> B s)
-  (bar : forall (s : nat -> A), {n : nat & B (restrict s n)}),
+  (bar : is_bar (@B)),
   B (seq_nil A).
 
 (** The statement that the type [A] satisfies bar induction. *)
 Definition bar_induction (A : Type) :=
   forall (B : forall {n : nat} (s : Seq A n), Type)
-  (ispred : forall {n : nat} (s : Seq A n), IsHProp (B s))
+  (isprop : forall {n : nat} (s : Seq A n), IsHProp (B s))
   (ind : forall (n : nat) (s : Seq A n), (forall (a : A), B (append s a)) -> B s)
-  (bar : forall (s : nat -> A), {n : nat & B (restrict s n)}),
+  (bar : is_bar (@B)) ,
   B (seq_nil A).  
 
 Definition decidable_fan_theorem (A : Type) :=
   forall (B : forall {n : nat} (s : Seq A n), Type)
-  (ispred : forall {n : nat} (s : Seq A n), IsHProp (B s))
+  (isprop : forall {n : nat} (s : Seq A n), IsHProp (B s))
   (dec : forall {n : nat} (s : Seq A n), Decidable (B s))
-  (bar : forall (s : nat -> A), {n : nat & B (restrict s n)}),
+  (bar : is_bar (@B)) ,
   {M : nat & forall (s : nat -> A), {n : (nat_seq M) & B (restrict s n.1)}}.
 
 Definition fan_theorem (A : Type) :=
   forall (B : forall {n : nat} (s : Seq A n), Type)
-  (bar : forall (s : nat -> A), {n : nat & B (restrict s n)}),
+  (bar : is_bar (@B)) ,
   {M : nat & forall (s : nat -> A), {n : (nat_seq M) & B (restrict s n.1)}}.
 
 (** The family we use to apply the fan theorem in our proof that continuity implies uniform continuity *)
 Definition uq_theorem_family {A : Type} (p : (nat -> A) -> Bool) 
-  : forall (n : nat) (s : Seq A n), Type.
-Proof.
-  intros n s.
-  exact (forall (u v : nat -> A) (h : restrict u n = s), u =[n] v -> p u = p v).
-Defined.
+  : forall (n : nat) (s : Seq A n), Type
+  := fun n s => 
+      (forall (u v : nat -> A) (h : restrict u n = s), u =[n] v -> p u = p v).
 
 Definition is_bar_uq_theorem_family {A : Type} 
   (p : (nat -> A) -> Bool) (conn : is_continuous p)
-  : forall (s : nat -> A), {n : nat & uq_theorem_family p n (restrict s n)}.
+  : is_bar (@uq_theorem_family A p).
 Proof.
   intro s.
   specialize (conn s 1).
@@ -85,22 +126,20 @@ Proof.
   unfold uq_theorem_family.
   intros u v h.
   assert (d : u =[conn.1] s).
-  - (* Incomplete step, searching for the best way to go between =[n] and restrict. 
-       Ideally want a theorem (u =[n] v) <-> (restrict u n = restrict v n). *)
-    admit. (* jdc: this line need to allow the file to build *)
+  - exact (seq_agree_res (apD10 h)).
   - intro t.
     set (l := us_symmetric conn.1 _ _ d).
     set (l' := us_transitive conn.1 _ _ _ l t).
     exact ((conn.2 _ l)^ @ (conn.2 _ l')).
-Admitted.  
+Defined.    
 
 (** The fan theorem implies that every continuous function is uniformly continuous.
-Current proof uses the full fan theorem and is incomplete. Less powerful versions might be enough, e.g. a fan theorem for predicates of type forall {n : nat} (s : Seq A n), Bool *)
-Definition uq_theorem (fan : fan_theorem Bool) 
-  (p : (nat -> Bool) -> Bool) (conn : is_continuous p)
+Current proof uses the full fan theorem. Less powerful versions might be enough, e.g. a fan theorem for predicates of type forall {n : nat} (s : Seq A n), Bool *)
+Definition uq_theorem {A : Type} (fan : fan_theorem A) 
+  (p : (nat -> A) -> Bool) (conn : is_continuous p)
   : is_uniformly_continuous p.
 Proof.
-  set (fanapp := fan (uq_theorem_family p) (is_bar_uq_theorem_family p conn)).
+  set (fanapp := fan (@uq_theorem_family A p) (is_bar_uq_theorem_family p conn)).
   exists fanapp.1.
   intros u v h.
   set (c := conn u).
@@ -109,5 +148,5 @@ Proof.
   unfold uq_theorem_family in e.
   apply e.
   - reflexivity.
-  - (* Incomplete step (similar to the one above), follows from h and hm. *)
-Admitted.  
+  - exact (us_rel_leq _ h).
+Defined.  
