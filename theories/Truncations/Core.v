a914d5a665b949936c305a95e7d7569f6c527d7f
thchatzidiamantis <tchtc@proton.me>
2025-06-21 20:22:22 -0400

    up to date

--- theories/Truncations/Core.v
+++ theories/Truncations/Core.v
@@ -343,22 +343,6 @@ Proof.
     reflexivity.
 Defined.
 
-(** ** Tactic to remove truncations in hypotheses if possible *)
-
-Ltac strip_truncations :=
-  (** search for truncated hypotheses *)
-  progress repeat
-    match goal with
-    | [ T : _ |- _ ]
-      => revert_opaque T;
-        refine (@Trunc_ind _ _ _ _ _);
-        (** ensure that we didn't generate more than one subgoal, i.e. that the goal was appropriately truncated *)
-        [];
-        intro T
-  end.
-
-(** See [strip_reflections] and [strip_modalities] for generalizations to other reflective subuniverses and modalities.  We provide this version because it sometimes needs fewer universes (due to the cumulativity of [Trunc]).  However, that same cumulativity sometimes causes free universe variables.  For a hypothesis of type [Trunc@{i} X], we can use [Trunc_ind@{i j}], but sometimes Coq uses [Trunc_ind@{k j}] with [i <= k] and [k] otherwise free.  In these cases, [strip_reflections] and/or [strip_modalities] may generate fewer universe variables. *)
-
 (** ** Iterated truncations *)
 
 (** Compare to [O_leq_Tr] and [O_strong_leq_Tr] in SeparatedTrunc.v. *)
