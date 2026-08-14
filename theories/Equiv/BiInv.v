062db1abb6c38e8661db5464db357b892c07b60a
heimerzieher <heimerzieher@me.com>
2026-08-14 16:02:20 +0200

    BiInv: clean up biinv_compat* results
    Co-authored-by: Dan Christensen <jdc@uwo.ca>

--- theories/Equiv/BiInv.v
+++ theories/Equiv/BiInv.v
@@ -132,12 +132,13 @@ Proof.
   by nrefine (Build_BiInv A A idmap (Build_IsBiInv A A idmap idmap idmap _ _)).
 Defined.
 
-(** Assume we have a commutative square [e' o f == g o e] in which [e] and [e'] are bi-invertible.  Then [f] and [g] also commute with the retractions and sections, and the homotopies in these new squares each satisfy a coherence condition. *)
+(** Assume we have a commutative square [g o e == e' o f] in which [e] and [e'] are bi-invertible.  Then [f] and [g] also commute with the retractions and sections, and the homotopies in these new squares each satisfy a coherence condition. *)
 
 Section EquivalenceCompatibility.
 
   Context {A B C D : Type}.
   Context (e : BiInv A B) (e' : BiInv C D) (f : A -> C) (g : B -> D).
+  Context (pe : g o e == e' o f).
 
   Let s := sect_biinv e.
   Let r := retr_biinv e.
@@ -148,51 +149,37 @@ Section EquivalenceCompatibility.
   Let re' := eissect_biinv e' : r' o e' == idmap.
   Let es' := eisretr_biinv e' : e' o s' == idmap.
 
-  Definition helper_r (pe : e' o f == g o e) : r' o g o e == f o r o e.
-  Proof.
-    intro x.
-    exact ((ap r' (pe x))^ @ (re' (f x) @ (ap f (re x))^)).
-  Defined.
-
-  Definition helper_s (pe : e' o f == g o e) : e' o s' o g == e' o f o s.
-  Proof.
-    intro y.
-    exact (es' (g y) @ (ap g (es y))^ @ (pe (s y))^).
-  Defined.
-
   (** The following lemmas express the coherence conditions mentioned above. *)
 
-  Definition biinv_compat_pr (pe : forall (x : A), e' (f x) = g (e x))
-    : r' o g == f o r.
+  Definition biinv_compat_pr : r' o g == f o r.
   Proof.
     rapply (equiv_ind e).
-    apply (helper_r pe).
+    exact (fun a => ap r' (pe a) @ (re' (f a) @ (ap f (re a))^)).
   Defined.
 
-  Definition biinv_compat_ps (pe : forall (x : A), e' (f x) = g (e x))
-    : s' o g == f o s.
+  Definition biinv_compat_ps : s' o g == f o s.
   Proof.
-    intro y.
+    intro b.
     apply (equiv_inj e').
-    apply (helper_s pe).
+    exact (es' (g b) @ (ap g (es b))^ @ pe (s b)).
   Defined.
 
-  Definition biinv_compat_pre (pe : forall (x : A), e' (f x) = g (e x)) (x : A)
-    : re' (f x) = ap r' (pe x) @ (biinv_compat_pr pe) (e x) @ ap f (re x).
+  Definition biinv_compat_pre (x : A)
+    : re' (f x) = (ap r' (pe x))^ @ biinv_compat_pr (e x) @ ap f (re x).
   Proof.
     unfold biinv_compat_pr.
     rewrite equiv_ind_comp.
     apply moveL_pM.
-    apply moveL_Mp.
+    apply moveL_Vp.
     reflexivity.
   Defined.
 
-  Definition biinv_compat_pes (pe : forall (x : A), e' (f x) = g (e x)) (y : B)
-    : es' (g y) = ap e' ((biinv_compat_ps pe) y) @ pe (s y) @ ap g (es y).
+  Definition biinv_compat_pes (y : B)
+    : es' (g y) = ap e' (biinv_compat_ps y) @ (pe (s y))^ @ ap g (es y).
   Proof.
     rewrite ap_equiv_inj.
     apply moveL_pM.
-    apply moveL_pM.
+    apply moveL_pV.
     reflexivity.
   Defined.
 
